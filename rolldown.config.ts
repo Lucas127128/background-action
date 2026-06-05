c05ba4c3522681fe6c2cc638693a649160b278b0
Lucas Leung <kayauleung2024@gmail.com>
2026-06-05 22:17:24 +0800

    Use rolldown instead of esbuild

---
+++ rolldown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig, RolldownPlugin } from 'rolldown';
+import Sonda from 'sonda/rolldown';
+
+export default defineConfig({
+  input: 'index.js',
+  platform: 'node',
+  plugins: [Sonda({ open: false }) as RolldownPlugin],
+  output: {
+    dir: 'dist',
+    format: 'cjs',
+    minify: true,
+    sourcemap: true,
+  },
+});
